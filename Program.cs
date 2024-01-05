dd63ec2e78740c6a61c24a91f619e9d5adb16630
Natchke <mariamnatchkebia29@gmail.com>
2024-01-06 00:59:24 +0400

    meate davaleba

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace meate_davaleba
+{
+    class Program
+    {
+        static void Main()
+        {
+             FileWorker textFileWorker = new TextFileWorker(128);
+            textFileWorker.Write();
+            textFileWorker.Read();
+            textFileWorker.Delete();
+            textFileWorker.Edit();
+
+             }
+           
+        }
+    public abstract class FileWorker
+    {
+        protected int MaxFileSize { get; }
+
+        public abstract string FileExtension { get; }
+
+        protected FileWorker(int maxFileSize)
+        {
+            MaxFileSize = maxFileSize;
+        }
+
+        public abstract void Read();
+        public abstract void Write();
+        public abstract void Edit();
+        public abstract void Delete();
+    }
+    public class TextFileWorker : FileWorker
+    {
+        public TextFileWorker(int maxFileSize) : base(maxFileSize) { }
+
+        public override string FileExtension
+        {
+            get { return ".txt"; }
+        }
+
+        public override void Read()
+        {
+            Console.WriteLine("I Can read from " + FileExtension + " file with max storage " + MaxFileSize);
+        }
+
+        public override void Write()
+        {
+            Console.WriteLine("I Can write to " + FileExtension + " file with max storage " + MaxFileSize);
+        }
+
+        public override void Edit()
+        {
+            Console.WriteLine("I Can edit " + FileExtension + " file with max storage " + MaxFileSize);
+        }
+
+        public override void Delete()
+        {
+            Console.WriteLine("I Can delete from " + FileExtension + " file with max storage " + MaxFileSize);
+
+
+        }
+    }
+
+}
---
+++ meore.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        FinanceOperations bankLoan = new Bank();
+        FinanceOperations microFinanceLoan = new MicroFinance();
+        double loanAmountBank = bankLoan.CalculateLoanPercent(12, 1000);
+        double loanAmountMicroFinance = microFinanceLoan.CalculateLoanPercent(12, 1000);
+    }
+    public interface FinanceOperations
+    {
+        bool CheckUserHistory();
+        double CalculateLoanPercent(int month, double amountPerMonth);
+    }
+    public class Bank : FinanceOperations
+    {
+        public bool CheckUserHistory()
+        {
+            Random random = new Random();
+            return random.Next(2) == 0;
+        }
+        public double CalculateLoanPercent(int month, double amountPerMonth)
+        {
+            if (CheckUserHistory())
+            {
+                double maxAmount = amountPerMonth * month * 1.05; // 5% 
+                Console.WriteLine($"Bank Loan approved. Total amount to be paid: {maxAmount:C}");
+                return maxAmount;
+            }
+            else
+            {
+                Console.WriteLine("Bank Loan denied.");
+                return 0;
+            }
+        }
+    }
+    public class MicroFinance : FinanceOperations
+    {
+        public bool CheckUserHistory()
+        {
+            return true;
+        }
+
+        public double CalculateLoanPercent(int month, double amountPerMonth)
+        {
+            double maxAmount = amountPerMonth * month * 1.1 + 4; // 10% + 4$
+            Console.WriteLine($"MicroFinance Loan approved. Total amount to be paid: {maxAmount:C}");
+            return maxAmount;
+        }
+    }
+}
